3dbe10e9916613292294e788c43359125ac65448
Manuel Aguilera <maguileraespejo@gmail.com>
2024-03-22 22:09:33 -0300

    Commiting project

---
+++ src/main/java/com/pucv/avancecurricular/AvanceCurricular.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pucv.avancecurricular;
+
+/**
+ *
+ * @author magui
+ */
+public class AvanceCurricular {
+
+    public static void main(String[] args) {
+        System.out.println("Falta hacer clases");
+    }
+}
