6b5e3336282810150852aefff7b1d9d08d782845
Alex <alfaroalex700@hotmail.com>
2024-03-24 10:30:13 -0300

    prototipo menu

--- src/main/java/com/pucv/avancecurricular/AvanceCurricular.java
+++ src/main/java/com/pucv/avancecurricular/AvanceCurricular.java
@@ -3,7 +3,7 @@
  */
 
 package com.pucv.avancecurricular;
-
+import java.util.Scanner;
 /**
  *
  * @author magui
@@ -11,6 +11,36 @@ package com.pucv.avancecurricular;
 public class AvanceCurricular {
 
     public static void main(String[] args) {
-        System.out.println("Falta hacer clases");
+        int opcion;
+        Scanner scan=new Scanner(System.in);
+        do{
+            System.out.println("*****************************");
+            System.out.println("*           menu            *");
+            System.out.println("*****************************");
+            System.out.println("* 1.-agregar alumno         *");
+            System.out.println("* 2.-eliminar alumno        *");
+            System.out.println("* 3.-ver alumnos            *");
+            System.out.println("* 4.- ver progreso de alumno*");
+            System.out.println("*5.-salir                   *");
+            System.out.println("*****************************");
+            opcion=scan.nextInt();
+            switch(opcion){
+                case 1:
+                    //agregar metodos
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("ingrese una opcion correcta");
+                
+                
+            }
+            
+        }while(opcion!=5);
+        
     }
 }
