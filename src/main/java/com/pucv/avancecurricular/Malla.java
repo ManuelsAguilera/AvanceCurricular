c339aafb784411caff3d7a775406a7a096a757f7
Alex <alfaroalex700@hotmail.com>
2024-03-26 15:52:24 -0300

    Malla setters and getters

--- src/main/java/com/pucv/avancecurricular/Malla.java
+++ src/main/java/com/pucv/avancecurricular/Malla.java
@@ -4,10 +4,33 @@
  */
 package com.pucv.avancecurricular;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author alfar
  */
 public class Malla {
+    private ArrayList<Asignatura> listaAsignaturas;
+    
+    public Malla(ArrayList<Asignatura> listaAsignaturas) {
+        this.listaAsignaturas = listaAsignaturas;
+    }
+
+    public ArrayList<Asignatura> getListaAsignaturas() {
+        return listaAsignaturas;
+    }
+    
+    public void setListaAsignaturas(ArrayList<Asignatura> listaAsignaturas) {
+        this.listaAsignaturas = listaAsignaturas;
+    }
+
+    public void agregarAsignatura(Asignatura asignatura) {
+        if (listaAsignaturas == null) {
+            listaAsignaturas = new ArrayList<>();
+        }
+        listaAsignaturas.add(asignatura);
+    }
+    
     
 }
