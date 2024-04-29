4e5647f3386a4cbe820fa923cdd1651007ffca05
manuelsaguilera <maguileraespejo@gmail.com>
2024-04-29 09:26:13 -0400

    Agregada tabla para alumnos

--- src/main/java/com/pucv/avancecurricular/vista/VistaAlumnosP.java
+++ src/main/java/com/pucv/avancecurricular/vista/VistaAlumnosP.java
@@ -4,6 +4,8 @@
  */
 package com.pucv.avancecurricular.vista;
 
+import javax.swing.table.DefaultTableModel;
+
 /**
  *
  * @author manu
@@ -13,10 +15,26 @@ public class VistaAlumnosP extends javax.swing.JPanel {
     /**
      * Creates new form VistaAlumnosP
      */
+    private DefaultTableModel model;
     public VistaAlumnosP() {
+        model = new DefaultTableModel();
+        model.addColumn("Nombre");
+        model.addColumn("Rut");
+        model.addColumn("Malla");
+        model.addColumn("Creditos");
         initComponents();
     }
-
+    
+    public void addRow(Object[] row)
+    {
+        model.addRow(row);
+    }
+    
+    public void resetRows()
+    {
+        for (int i =0; i < model.getRowCount(); i++)
+            model.removeRow(i);
+    }
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -26,19 +44,29 @@ public class VistaAlumnosP extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+
+        jTable1.setAutoCreateRowSorter(true);
+        jTable1.setModel(this.model);
+        jTable1.setFocusable(false);
+        jScrollPane1.setViewportView(jTable1);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 443, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 443, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 350, Short.MAX_VALUE)
+            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 350, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
